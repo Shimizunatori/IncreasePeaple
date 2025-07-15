6276735563920f5476a01a2c4e6d0703a12822d5
清水七鳳 <TTC22C1020052@edu.tech.ac.jp>
2025-07-15 15:59:22 +0900

    一旦ゲームマネージャー途中だけど

--- Assets/Member/Natori/GameManager.cs
+++ Assets/Member/Natori/GameManager.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField, Header("人間たち")] private List<GameObject> _peaple = new List<GameObject>();
+    [SerializeField, Header("生成する人間たち")] private List<GameObject> _peaple = new List<GameObject>();
+    [Header("場に存在する人間たち")] public List<GameObject> _peapleList = new List<GameObject>();
     [SerializeField, Header("最大人間数")] private int _maxPeaple;
+    [SerializeField, Header("ビルたち")] private List<GameObject> _buildList = new List<GameObject>();
     private int _peapleCount;
     public int _score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //_buildList
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var _randomPeaple = Random.Range(0, 4);
+        if (_peapleList.Count < _maxPeaple)
+        {
+            Debug.Log("Spawn");
+            var _person = Instantiate(_peaple[_randomPeaple], transform.position, Quaternion.identity);
+            _person.transform.parent = transform;
+            _peapleList.Add(_person);
+            _person.gameObject.GetComponent<SpriteRenderer>().sortingOrder = _randomPeaple;
+        }
     }
 }
